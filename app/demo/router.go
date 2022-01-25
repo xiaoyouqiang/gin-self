728615ff9d63fd47af0f7bf258f00e476c073120
qiangyouxiao <xqq_1988@163.com>
2022-01-25 21:40:25 +0800

    调整

--- app/demo/router.go
+++ app/demo/router.go
@@ -5,9 +5,7 @@ import (
 )
 
 func ReturnRoute(r *gin.Engine) {
-	//该方法需要登录验证， token检查中间件: middleware.CheckToken
 	r.Any("/demo/test/index", Index)
-	//r.GET("/demo/test/index1",middleware.CheckTokenMiddleWare(), Index1)
 	r.GET("/demo/test/index1", Index1)
 	r.GET("/demo/test/index2", Index2)
 	r.POST("/demo/test/index2", Index2)
