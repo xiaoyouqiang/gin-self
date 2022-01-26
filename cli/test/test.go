24baaf6be398b97913d9ba1535f6c4fd6f164333
qiangyouxiao <xqq_1988@163.com>
2022-01-26 22:56:31 +0800

    up

--- cli/test/test.go
+++ cli/test/test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"gin-self/core"
 	"gin-self/extend/self_db"
+	"gin-self/extend/utils/debug"
 	"gin-self/model/mysql/test1_model"
 )
 
@@ -12,18 +13,10 @@ type testStrut struct {
 }
 
 func Test(c *core.CliContext)  {
-	//query := test_model.NewQueryBuilder()
-	//result, _ := query.Select("id", "name", "created_time").WhereOp("id", self_db.Equal, 35).QueryOne()
-	//debug.VarDump(result)
-	//v,_ := c.GetParam("name")
-	//debug.VarDump(v)
 	query := test1_model.NewQueryBuilder()
-	//query.Select("id", "name", "created_time").WhereOp("id", self_db.Equal, 1).QueryOne()
+	query.Select("id", "name","goods_id").WhereOp("id", self_db.Equal, 114).QueryOne()
 
-	 query.Select("id", "name","goods_id").WhereOp("id", self_db.Equal, 114).QueryOne()
-
-	//debug.VarDump(result)
 	var t  = testStrut{}
 	c.BindParam(&t)
-	//debug.VarDump(t)
+	debug.VarDump(t)
 }
